ab94c01f0f2e716f616cf7020101aa38debc17c7
Gobato15 <gobato59@gmail.com>
2025-10-06 21:50:27 -0300

    Cadastro de Produtos
    Cadastro de Produtos

---
+++ src/main/java/Objetos/Produto.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+/**
+ *
+ * @author mauricio.rgobato
+ */
+public class Produto {
+    private String nome;
+    private String descricao;
+    private int quantidade;
+    private Double preco;
+    
+    public Produto(String nome, String descricao, int quantidade, Double preco) {
+        setNome(nome);
+        setDescricao(descricao);
+        setQuantidade(quantidade);
+        setPreco(preco);
+    }
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the preco
+     */
+    public Double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
